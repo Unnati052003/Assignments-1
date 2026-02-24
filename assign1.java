6e0f3e40f48bde63a69b4393e355f6122f233819
Unnati Warule <unnatiwarule@gmail.com>
2026-02-24 23:11:16 +0530

    Assignment 1

---
+++ assign1.java
@@ -0,0 +1,26 @@
+public class assign1
+{
+    public static void main(String[] args) 
+    {
+
+      
+        if (args.length == 0) {
+            System.out.println("Please enter numbers as command line arguments.");
+            return;
+        }
+
+     
+        int max = Integer.parseInt(args[0]);
+
+     
+        for (int i = 1; i < args.length; i++) {
+            int num = Integer.parseInt(args[i]);
+
+            if (num > max) {
+                max = num;
+            }
+        }
+
+        System.out.println("Maximum number is: " + max);
+    }
+}
